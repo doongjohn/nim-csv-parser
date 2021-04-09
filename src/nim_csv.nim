48198ba3c87ef7d3577bb1f9648216db7f86dc40
doongjohn <40219740+doongjohn@users.noreply.github.com>
2021-04-09 09:51:34 +0900

    Update nim_csv.nim

--- src/nim_csv.nim
+++ src/nim_csv.nim
@@ -1,8 +1,16 @@
+# TODO
+# - parse "" correctly
+# - do stuff better
+
+
 import types
 import parser
 
 
 proc main =
+  # stdout.write "csv file path: "
+  # let filepath = stdin.readLine()
+  # let source = readFile filepath
   let source = readFile "./csv-example.csv"
   let csvObj = parse source
   
